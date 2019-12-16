5f6aa1a53e2329b0be58070921f2e92dda5d95cc
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-12-15 17:09:24 -0800

    removed warning, thanks @conficient

--- src/BlazorTable/Components/Table.razor.cs
+++ src/BlazorTable/Components/Table.razor.cs
@@ -10,7 +10,7 @@ namespace BlazorTable
     public partial class Table<TableItem> : ITable<TableItem>
     {
         [Parameter(CaptureUnmatchedValues = true)]
-        public IDictionary<string, object> UnknownParameters { get; set; }
+        public IReadOnlyDictionary<string, object> UnknownParameters { get; set; }
 
         [Parameter]
         public string TableClass { get; set; } = "table table-striped table-bordered table-hover table-sm";
