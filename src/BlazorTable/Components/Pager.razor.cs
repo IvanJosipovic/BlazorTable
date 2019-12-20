9d411800a31841376e59a2e77956662ffc11255a
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2019-12-19 23:05:36 -0800

    added comments

--- src/BlazorTable/Components/Pager.razor.cs
+++ src/BlazorTable/Components/Pager.razor.cs
@@ -2,17 +2,30 @@
 
 namespace BlazorTable
 {
+    /// <summary>
+    /// BlazorTable Pager
+    /// </summary>
+    /// <typeparam name="TableItem"></typeparam>
     public partial class Pager<TableItem>
     {
         [CascadingParameter(Name = "Table")]
         public ITable<TableItem> Table { get; set; }
 
+        /// <summary>
+        /// Always show Pager, otherwise only show if TotalPages > 1
+        /// </summary>
         [Parameter]
         public bool AlwaysShow { get; set; }
 
+        /// <summary>
+        /// Show current page number
+        /// </summary>
         [Parameter]
         public bool ShowPageNumber { get; set; }
 
+        /// <summary>
+        /// Show total item count
+        /// </summary>
         [Parameter]
         public bool ShowTotalCount { get; set; }
     }
