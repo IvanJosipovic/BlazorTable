f5c52afb688f5cc6a9b1b55aa9462f5491aaf084
Mert Ussakli <meussakl@microsoft.com>
2022-06-28 13:41:49 -0700

    Upgrade to .net6, drop unused projects.

--- src/BlazorTable.Tests/BrowserTests.cs
+++ src/BlazorTable.Tests/BrowserTests.cs
@@ -24,7 +24,7 @@ namespace BlazorTable.Tests
             else
                 throw new Exception($"Missing {filename}");
 
-            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
 
             Browser = await Puppeteer.LaunchAsync(new LaunchOptions
             {
